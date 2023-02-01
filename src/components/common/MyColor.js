9d603a2c1eead46befd2651b123f669eb1ef9a8d
Yanni0923 <yannnni0923@gmail.com>
2023-02-02 00:13:07 +0800

    定義主要顏色

--- src/components/common/MyColor.js
+++ src/components/common/MyColor.js
@@ -1,5 +1,26 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-export const DeepBlue = "#2c4964";
-export const DeepPink = "#ff8ba7";
+// 主色系
+export const ColorDeepBlue = "#2c4964";
+export const ColorDeepPink = "#ff8ba7";
+export const ColorTextGray = "#6c757d";
+
+// 皮膚色：進度條用
+export const ColorLightOrange = "#ffc6c7";
+export const ColorSkin = "#fbeaf1";
+
+// 黑灰：工作經歷用
+export const ColorDeepGary = "#343A40";
+export const ColorGary = "#e4ebf3";
+
+// 深藍：工作經歷次標題用
+export const ColorDeepBlue2 = "#4b7dab";
+
+// 黑灰：Services 文字用
+export const ColorTextBlack = "#111111";
+// 黑灰：Portfolio 文字用
+
+export const ColorTextBlack2 = "#5a656e";
+// export const Color = "#";
+// export const Color = "#";
