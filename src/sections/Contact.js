0ce2469808445327cbd9d65d87565753f327ff12
Yanni0923 <yannnni0923@gmail.com>
2023-02-02 20:57:00 +0800

    各 section title 內容初設定

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,13 +1,25 @@
 import React from "react";
 import styled from "@emotion/styled";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Section from "../components/common/Section";
+import SectionTitle from "../components/common/SectionTitle";
+import SectionSubTitle from "../components/common/SectionSubtitle";
+
+const titleH2 = "Contact";
+const titlePara =
+  "Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.";
+const titleFront = "Contact Me";
+const titleBack = "Contact";
 
 const Contact = () => {
   return (
     <>
-      <h1>Contact</h1>
+      <Section>
+        <SectionTitle titleFront={titleFront} titleBack={titleBack} />
+        <SectionSubTitle h2={titleH2} para={titlePara} />
+        個別 Section 的內容放這裡
+      </Section>
     </>
   );
 };
-
 export default Contact;
--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -1,13 +1,25 @@
 import React from "react";
 import styled from "@emotion/styled";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Section from "../components/common/Section";
+import SectionTitle from "../components/common/SectionTitle";
+import SectionSubTitle from "../components/common/SectionSubtitle";
+
+const titleH2 = "Portfolio";
+const titlePara =
+  "Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.";
+const titleFront = "My Portfolio";
+const titleBack = "Portfolio";
 
 const Portfolio = () => {
   return (
     <>
-      <h1>Portfolio</h1>
+      <Section>
+        <SectionTitle titleFront={titleFront} titleBack={titleBack} />
+        <SectionSubTitle h2={titleH2} para={titlePara} />
+        個別 Section 的內容放這裡
+      </Section>
     </>
   );
 };
-
 export default Portfolio;
