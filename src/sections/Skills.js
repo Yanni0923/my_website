ede9250b20dc6d2cfec32b30c0e45d90d73d1d76
Yanni0923 <yannnni0923@gmail.com>
2023-02-09 02:48:20 +0800

    完成 Skill section 和裡面的動態 Progress bar

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -4,6 +4,11 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Section from "../components/common/Section";
 import SectionTitle from "../components/common/SectionTitle";
 import SectionSubTitle from "../components/common/SectionSubtitle";
+import {
+  ColorDeepBlue,
+  ColorSkin,
+  ColorLightOrange,
+} from "../components/common/MyColor";
 
 const titleH2 = "Skills";
 const titlePara =
@@ -11,13 +16,104 @@ const titlePara =
 const titleFront = "My Skills";
 const titleBack = "Skills";
 
+const StyledProgress = styled.div`
+  height: 50px;
+  display: block;
+  background: none;
+  margin: 40px 0;
+
+  .skill {
+    margin: 0 0 6px 0;
+    text-transform: uppercase;
+    display: block;
+    font-weight: 600;
+    font-family: "Poppins", sans-serif;
+    color: ${ColorDeepBlue};
+
+    .val {
+      float: right;
+      font-style: normal;
+    }
+  }
+
+  .progress-div {
+    background-color: ${ColorSkin};
+    border-radius: 0.5rem;
+
+    .progress {
+      background-color: ${ColorLightOrange};
+      height: 10px;
+      border-radius: 1rem;
+      transition: 1s ease;
+      transition-delay: 0.5s;
+    }
+  }
+`;
+
+const ProgressBar = ({ percent }) => {
+  const [value, setValue] = React.useState(0);
+
+  React.useEffect(() => {
+    setValue(percent);
+  });
+
+  return (
+    <div>
+      <div className="progress-div" style={{ width: "100%" }}>
+        <div style={{ width: `${value}%` }} className="progress" />
+      </div>
+    </div>
+  );
+};
+
+const ProgressComponent = (props) => {
+  return (
+    <StyledProgress>
+      <span className="skill">
+        {props.progressBarName}
+        <i className="val">{props.progressBarPercent}%</i>
+      </span>
+      <ProgressBar percent={props.progressBarPercent} />
+    </StyledProgress>
+  );
+};
+
 const Skills = () => {
   return (
     <>
       <Section>
         <SectionTitle titleFront={titleFront} titleBack={titleBack} />
         <SectionSubTitle h2={titleH2} para={titlePara} />
-        個別 Section 的內容放這裡
+        <div className="row skills-content">
+          <div className="col-lg-6">
+            <ProgressComponent
+              progressBarName={"HTML"}
+              progressBarPercent={100}
+            />
+            <ProgressComponent
+              progressBarName={"CSS"}
+              progressBarPercent={90}
+            />
+            <ProgressComponent
+              progressBarName={"JAVASCRIPT"}
+              progressBarPercent={75}
+            />
+          </div>
+          <div className="col-lg-6">
+            <ProgressComponent
+              progressBarName={"PHP"}
+              progressBarPercent={80}
+            />
+            <ProgressComponent
+              progressBarName={"WORDPRESS/CMS"}
+              progressBarPercent={90}
+            />
+            <ProgressComponent
+              progressBarName={"PHOTOSHOP55"}
+              progressBarPercent={55}
+            />
+          </div>
+        </div>
       </Section>
     </>
   );
